601e9e644e393425e809a8b26e6b8d5b225af9f0
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2024-08-28 19:00:54 +0200

    Mob Programming

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
+import org.gradle.kotlin.dsl.ktlint
+import org.gradle.kotlin.dsl.libs
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ktlint)
+}
+
+ktlint {
+    android = true
+    reporters {
+        reporter(ReporterType.CHECKSTYLE)
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "KtlintExample"
 include(":app")
- 
